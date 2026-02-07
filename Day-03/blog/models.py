ba92cb4f0e4caafff6daa716564e0ac1a6d9642d
ahmedmalik414 <ahmedmalik786414@gmail.com>
2026-02-07 10:44:53 +0500

    first commit of day 3 designed models

---
+++ Day-03/blog/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class Userprofile (models.Model):
+    
+    name= models.CharField(max_length=30)
+    age= models.IntegerField()
+    def __str__(self):
+        return self.name
+
+CATEGORY_CHOICES = [
+    ("A", "A"),
+    ("B", "B"),
+    ("C", "C"),
+]
+
+class Category(models.Model):
+    name = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Tags(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+class Blogpost(models.Model):  
+    title= models.CharField(max_length=100) 
+    content= models.TextField(max_length=1000)
+    tags= models.ManyToManyField(Tags)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    author= models.ForeignKey(Userprofile,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+    
+    
+
+    
+
+    
+
+    
+
+  
