0b9142d888fec11ed1cc10d54fbf75f54d630d07
ahmedmalik414 <ahmedmalik786414@gmail.com>
2026-02-08 10:20:33 +0500

    Day-03 completed practice ORM commands for CRUD operations, displayed the data on pages dynamically and completed hands-on practice with designing tables schemas

--- Day-03/blog/views.py
+++ Day-03/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Blogpost
+def show_posts(request):
+    posts= Blogpost.objects.all()
+    
+    return render(request, 'posts.html', {'posts': posts})
+    
